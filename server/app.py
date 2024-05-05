7b3fed616600de0f2dcf663290d592c949266b36
Sohel Mujawar <sohelmujawar1807@gmail.com>
2024-05-05 14:12:07 +0530

    modal function add

--- server/app.py
+++ server/app.py
@@ -1,46 +1,56 @@
-from fastapi import FastAPI, HTTPException
-from upstash_vector import Index
+from modal import Stub, build, enter, method, web_endpoint,Image
 from typing import Dict
 import os
-from os import getenv
-from dotenv import load_dotenv
-load_dotenv()
-app = FastAPI()
+import modal
 
+image = Image.debian_slim().pip_install(
+    "upstash_vector"
 
-URL = os.getenv("Url")
-Token = os.getenv("Token")
+)
+stub = Stub(name="resume-parser", image=image)
 
-
-
-index = Index(url=URL, token=Token)
-
-@app.post("/upsert/")
-async def upsert_data(requestData: Dict):
-    try:
-        data_list = requestData["data"]
-        client_id = requestData["client"]
-        for data in data_list:
-            application_id = data.get("applicationId", "")
-            content = data.get("content", "")
-            index.upsert(
-                vectors=[
+@stub.function(secrets=[modal.Secret.from_name("Url"), modal.Secret.from_name("Token")],timeout=1000)
+@web_endpoint(label="upsert",method="POST")
+def upsert_data(requestData: Dict):
+    from upstash_vector import Index
+    data_list = requestData["data"]
+    client_id = requestData["client"]
+        
+    URL = os.environ["Url"] 
+    Token = os.environ["Token"] 
+        
+    index = Index(url=URL, token=Token)
+    for data in data_list:
+        application_id = data.get("applicationId", "")
+        content = data.get("content", "")
+        index.upsert(
+            vectors=[
                     (f"{client_id}_{application_id}", content, {"clientID": client_id, "applicationId": application_id, "content": content}),
                 ]
             )
         return {"message": "Data upserted successfully"}
-    except KeyError as e:
-        raise HTTPException(status_code=400, detail=f"Missing required key: {e}")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
+@stub.function()
+@web_endpoint(label="match",method="POST")
+def start_match(requestData: Dict):
+    client=requestData["client"]
+    data = requestData["data"]
+    return (Model.query_data.remote(client,data))
+    
+    
+@stub.cls(secrets=[modal.Secret.from_name("Url"), modal.Secret.from_name("Token")])
+class Model:
+    @build()
+    @enter()
+    def start_model(self):
+        from upstash_vector import Index
+        URL = os.environ["Url"] 
+        Token = os.environ["Token"] 
+        self.index = Index(url=URL, token=Token)
 
-@app.post("/query/")
-async def query_data(requestData: Dict):
-    try:
-        client=requestData["client"]
-        data = requestData["data"]
-        result = index.query(
+    @method()
+    def query_data(self,client:str,data:str):
+        result = self.index.query(
             data=data,
             top_k=5,
             include_vectors=False,
@@ -51,11 +61,8 @@ async def query_data(requestData: Dict):
         for chunk in result:
             if chunk.metadata.get("clientID") == client:
                 temp={"Application_id":chunk.metadata.get("applicationId"),"Score":chunk.score,"Content":chunk.metadata.get("content")}
-            else:
-                return {"result":"NO data found"}
             info.append(temp)
+        if not info:
+            return {"result":"NO data found"}
+            
         return {"result":info}
-    except KeyError as e:
-        raise HTTPException(status_code=400, detail=f"Missing required key: {e}")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
