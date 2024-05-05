10a43e044d1a5fed588a55f06d5e7cb6a93721f4
sahil <sahilmulani501@gmail.com>
2024-05-05 13:28:54 +0530

    test deployment

--- server/app.py
+++ server/app.py
@@ -1,20 +1,21 @@
 # api to get client information
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from model import startModel  # Correct import
 
 
 
 
 class InputModel(BaseModel):
     query: str
-    client: str
+    url: str
 
 app = FastAPI()
 
 @app.post('/')
 async def test_api(payload: InputModel):
-    return {"result": "helloo"}
+    res = await startModel(payload.url, payload.query)
+    return {"result": res}
 
 @app.get('/')
 async def home():
