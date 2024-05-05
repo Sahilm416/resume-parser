39df0362973e4feddd8fe4f2ecb9ac3ea2293f86
Sohel Mujawar <sohelmujawar1807@gmail.com>
2024-05-05 19:46:26 +0530

    application id changes

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from modal import Stub, build, enter, method, web_endpoint,Image
 from typing import Dict
 import os
 import modal
-
+application_id_dict={}
 image = Image.debian_slim().pip_install(
     "upstash_vector"
 
@@ -18,16 +18,24 @@ def upsert_data(requestData: Dict):
         
     URL = os.environ["Url"] 
     Token = os.environ["Token"] 
+
+    if client_id in application_id_dict:
+        current_application_id = application_id_dict[client_id]
+    else:
+        current_application_id = 0
+        application_id_dict[client_id] = current_application_id
         
     index = Index(url=URL, token=Token)
     for data in data_list:
         application_id = data.get("applicationId", "")
         content = data.get("content", "")
+        information=f"{client_id}{application_id}{content}"
         index.upsert(
             vectors=[
-                    (f"{client_id}_{application_id}", content, {"clientID": client_id, "applicationId": application_id, "content": content}),
+                    (f"{client_id}_{current_application_id}", information, {"clientID": client_id, "applicationId": current_application_id, "content": content}),
                 ]
             )
+        application_id_dict[client_id] += 1
         return {"message": "Data upserted successfully"}
 
 @stub.function()
@@ -50,6 +58,7 @@ class Model:
 
     @method()
     def query_data(self,client:str,data:str):
+        data=f"{client}{data}"
         result = self.index.query(
             data=data,
             top_k=5,
@@ -61,7 +70,7 @@ class Model:
         for chunk in result:
             if chunk.metadata.get("clientID") == client:
                 temp={"Application_id":chunk.metadata.get("applicationId"),"Score":chunk.score,"Content":chunk.metadata.get("content")}
-            info.append(temp)
+                info.append(temp)
         if not info:
             return {"result":"NO data found"}
             
