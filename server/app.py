c80796b47294fc55b61f2000be79c7bd568a070a
Sohel Mujawar <sohelmujawar1807@gmail.com>
2024-05-06 20:27:11 +0530

    Minor changes

--- server/app.py
+++ server/app.py
@@ -18,25 +18,27 @@ def upsert_data(requestData: Dict):
         
     URL = os.environ["Url"] 
     Token = os.environ["Token"] 
-
+    app_id=0
     if client_id in application_id_dict:
         current_application_id = application_id_dict[client_id]
+        app_id=current_application_id
     else:
         current_application_id = 0
         application_id_dict[client_id] = current_application_id
+        app_id=current_application_id
         
     index = Index(url=URL, token=Token)
     for data in data_list:
         Email = data.get("Email", "")
         content = data.get("content", "")
-        information=f"{client_id}{current_application_id}{content}"
+        information=f"{client_id}{app_id}{content}"
         index.upsert(
             vectors=[
-                    (f"{client_id}_{current_application_id}", information, {"clientID": client_id, "Email": Email, "content": content}),
+                    (f"{client_id}_{app_id}", information, {"clientID": client_id, "Email": Email, "content": content}),
                 ]
             )
         application_id_dict[client_id] += 1
-        return {"message": "Data upserted successfully"}
+    return {"message": "Data upserted successfully"}
 
 @stub.function()
 @web_endpoint(label="match",method="POST")
@@ -58,9 +60,11 @@ class Model:
 
     @method()
     def query_data(self,client:str,data:str):
+        filter = f"clientID = '{client}'" 
         data=f"{client}{data}"
         result = self.index.query(
             data=data,
+            filter=filter,
             top_k=5,
             include_vectors=False,
             include_metadata=True
@@ -68,9 +72,8 @@ class Model:
         
         info=[]
         for chunk in result:
-            if chunk.metadata.get("clientID") == client:
-                temp={"Email":chunk.metadata.get("Email"),"Content":chunk.metadata.get("content"),"Score":chunk.score}
-                info.append(temp)
+            temp={"Email":chunk.metadata.get("Email"),"Content":chunk.metadata.get("content"),"Score":chunk.score}
+            info.append(temp)
         if not info:
             return {"result":"NO data found"}
             
