5ab982ac5cf9b063c29b8a708bdca7a498039e62
babgondpatil <babgondpatil2211@gmail.com>
2024-07-02 14:29:58 +0530

    add : gemini api for ai

--- website/app/api/ai/route.ts
+++ website/app/api/ai/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
-import { OpenAI } from "openai";
+import {GoogleGenerativeAI} from "@google/generative-ai"
+
 export const runtime = "edge";
 const instructions =
   "You are an ai that converts the given string resume into an HTML document. You dont respond a single word or character than the HTML resume you keep the Format of the returned HTML consistant for all the requests. You dont give HTML from html tag or body tag or head tag. You only give the presentation of given string into HTML string. Keep that in mind that your whole response is gonna embedded as html in website. Also add inline styles to prsent it more professionally.It should look like a proper resume. Style for allignment and good looking as this is gonna be presented in front of rcrutement team";
+
 export async function POST(req: Request) {
   const { resume } = (await req.json()) as { resume: string };
   if (!resume) {
@@ -15,22 +17,21 @@ export async function POST(req: Request) {
     );
   }
 
-  const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY! });
+  const genAI = new GoogleGenerativeAI(process.env.AI_KEY!);
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: instructions },
-      { role: "user", content: resume },
-    ],
-    stream: false,
-  });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" , systemInstruction: instructions});
+
+  const result = await model.generateContent(resume);
+  const response = await result.response;
+  const text = response.text();
+
+  // console.log(text)
 
   return NextResponse.json(
     {
       success: true,
       message: "generated the HTML response",
-      html: response.choices[0].message.content,
+      html: text,
     },
     { status: 200 }
   );
